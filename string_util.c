b618e6c0509cac1d23893f760f2249f508efa36c
kelvin34501 <kelvin34501@163.com>
2018-04-22 22:49:18 +0800

    kmp

--- string_util.c
+++ string_util.c
@@ -26,3 +26,48 @@ void string_split(char * str, vector_string * vec)
                 }
         }
 }
+
+char * kmp(char * src, char * tgt)
+{
+        int i=0,j=0,k=0;
+
+        //scan the tgt to get the length of offset array
+        for(k=0; tgt[k]!='\0'; k++);
+        if(k==0)
+                return NULL;
+        int * buf = malloc(sizeof(int) * k);
+
+        // gen offset array
+        buf[0]=-1; k=0;
+        for(i=1; tgt[i]!='\0'; i++) {
+                if(tgt[k]==tgt[i]) {
+                        buf[i] = buf[k]; i++; k++;
+                } else {
+                        buf[i] = k;
+                        k = buf[k];
+                        while(k>=0 && tgt[i]!=tgt[k])
+                                k = buf[k];
+                        i++;k++;
+                }
+        }
+        buf[i]=k;
+
+        //pattern matching
+        for(i=0,j=0; src[i]!='\0';) {
+                if(src[i]==tgt[j]) {
+                        i++; j++;
+                        if(tgt[j]=='\0')
+                                return (src)+i-j;
+                } else {
+                        j=buf[j];
+                        if(j<0) {
+                                i++; j++;
+                        }
+                }
+        }
+
+
+        //free the resources
+        free(buf);
+        return NULL;
+}
